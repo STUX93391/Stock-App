b94cd0b7c00bb82ae2e0b4169e1d1c4837837f57
rip <rip@gmail.com>
2021-01-21 20:54:08 +0500

    modified:   app/Http/Livewire/WithDraw.php

--- app/Http/Livewire/WithDraw.php
+++ app/Http/Livewire/WithDraw.php
@@ -14,16 +14,22 @@ class WithDraw extends Component
         ]);
         $acc=auth()->user()->business->account;
         if($acc->balance>500){
-            if($validated['amount']<$acc->balance){
-                auth()->user()->business->account->decrement('balance',$validated['amount']);
-                $this->amount='';
-                session()->flash('success','Rs' .$validated['amount'].' withdrawn successfully.');
-                return redirect()->route('account.create');
-            }else{
-                session()->flash('warning','Account balance is not enough for the transaction.');
-                return redirect()->route('account.create');
+            if(($acc->balance-$validated['amount'])>500){
+                if($validated['amount']<$acc->balance){
+                    auth()->user()->business->account->decrement('balance',$validated['amount']);
+                    $this->amount='';
+                    session()->flash('success','Rs' .$validated['amount'].' withdrawn successfully !!');
+                    return redirect()->route('account.create');
+                }else{
+                    session()->flash('warning','Account balance is not enough for the transaction !!');
+                    return redirect()->route('account.create');
 
+                }
+            }else{
+                session()->flash('warning','Account must remain with Rs 500 after the transaction !!');
+                return redirect()->route('account.create');
             }
+
         }else{
             session()->flash('warning','Account is almost empty.');
             return redirect()->route('account.create');
