2eede7b4a4a55220b10d9f5d18f8f5d4ec193a43
rip <rip@gmail.com>
2021-01-30 00:38:21 +0500

    modifiedwelcomepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,24 +12,52 @@
 
 
     </head>
-    <body>
-        <div class="py-5 text-center">
-            <h1 class=" text-danger">Stock Managment App</h1>
-            <div class="">
-                @if (Route::has('login'))
-                    <div class="">
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="btn btn-dark">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="btn btn-dark">Login</a>
+    <body style="background-color:#f1f0f0">
+        <div class="px-4 py-5">
+            <header >
+                <h1 class="text-danger"><strong>Stock Managment App</strong>
+                    <div class="float-right">
+                        @if (Route::has('login'))
+                            <div class="">
+                                @auth
+                                    <a href="{{ url('/dashboard') }}" class="btn btn-dark">Dashboard</a>
+                                @else
+                                    <a href="{{ route('login') }}" class="btn btn-dark">Login</a>
 
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="btn btn-dark">Register</a>
-                            @endif
-                        @endauth
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" class="btn btn-danger">Register</a>
+                                    @endif
+                                @endauth
+                            </div>
+                        @endif
                     </div>
-                @endif
+                </h1>
+            </header>
+
+            <div class="container py-5">
+                <div class="shadow card col-sm-5" style="background-color:#dddddd">
+                    <div class="card-body">
+                       <header class="">
+                           <h5>Tired of maintaining the stock ?</h5>
+                           <p class=""><i>&nbsp &nbsp Let's manage the stock on a click.</i></p>
+                           <p class="">To make your work effective and yourself relaxed </p>
+                           <p class="">add your business here.</p>
+                       </header>
+                    </div>
+                </div>
             </div>
+
+            <footer class="mt-5 text-secondary bg-dark">
+                <div class="container">
+                <h3 ><i>Contact</i></h3>
+                    <ul class="">
+                        <li class=""><h5 class="">email: _________</h5></li>
+                        <li class=""><h5 class="">Fax: _________</h5></li>
+                        <li class=""><h5 class="">Phone #: _________</h5></li>
+                    </ul>
+
+                </div>
+            </footer>
         </div>
 
     </body>
